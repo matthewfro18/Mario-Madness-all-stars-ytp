f2b97819f2043e95dbbf0ec519ea2cf108ffae7a
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-07-30 09:06:49 -0400

    Update setup-unix.sh

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -3,14 +3,14 @@
 #
 # REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
 # https://haxe.org/download/version/4.2.5/
-haxelib install lime
+haxelib install lime 7.9.0
 haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
+haxelib install flixel 5.2.1
+haxelib install flixel-addons 3.0.2
 haxelib install flixel-ui
 haxelib install flixel-tools
 haxelib install SScript
-haxelib install hxCodec
+haxelib install hxCodec 2.6.1
 haxelib install tjson
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
